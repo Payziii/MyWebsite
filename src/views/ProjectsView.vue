20c749aaefc044be5efdf5b76a238de8e6271ecc
Payziii <velmikhail123@gmail.com>
2025-12-13 10:55:16 +0500

    Полный список проектов

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -9,7 +9,7 @@ const projects = ref([
     avatar: '/logos/projects/payzibot.png',
     description: 'Простой <span class="green">Discord-бот</span>, созданный в 2019 году. Присутствует система модерации, игр, уровней, розыгрышей.',
     isActive: true,
-    shadowColor: '#3FCC65', 
+    shadowColor: 'rgba(50, 100, 255, 0.1)', 
     buttons: [
       { label: 'GitHub', url: 'https://github.com/Payziii/PayziBot', icon: '/logos/github.png' },
       { label: 'Добавить', url: 'https://discord.com/api/oauth2/authorize?client_id=576442351426207744&permissions=1411299798102&scope=bot', icon: '/logos/discord.png' },
@@ -22,7 +22,7 @@ const projects = ref([
     avatar: '/logos/projects/fiftytools.png',
     description: '<span class="green">Веб-сайт</span> с небольшим количеством полезных (и не очень) инструментов: погода, whois, рандомайзер.',
     isActive: true,
-    shadowColor: '#FBCBBB',
+    shadowColor: 'rgba(50, 100, 255, 0.1)',
     buttons: [
       { label: 'Telegram', url: 'https://t.me/FiftyToolsBot', icon: '/logos/tg.png' },
       { label: 'Сайт', url: 'https://tools.fifty.su', icon: '/logos/site.png' }
@@ -34,7 +34,7 @@ const projects = ref([
     avatar: '/logos/projects/fiftychat.png',
     description: 'Большой сборник <span class="green">нейросетей</span> с удобным интерфейсом для взаимодействия с ними.',
     isActive: true,
-    shadowColor: '#5CB85C',
+    shadowColor: 'rgba(50, 100, 255, 0.1)',
     buttons: [
       { label: 'Сайт', url: 'https://chat.fifty.su', icon: '/logos/site.png' },
       { label: 'RuStore', url: 'https://www.rustore.ru/catalog/app/su.fifty.chat', icon: '/logos/download.png' }
@@ -64,16 +64,114 @@ const projects = ref([
   },
   {
     id: 6,
+    name: 'FiftyFilter',
+    avatar: '/logos/projects/fiftyfilter.png',
+    description: 'Небольшой <span class="green">Telegram-бот</span> для обработки входящих заявок в чат.',
+    isActive: true,
+    shadowColor: 'rgba(50, 100, 255, 0.1)',
+    buttons: [
+      { label: 'Telegram', url: 'https://t.me/FiftyFilterBot', icon: '/logos/tg.png' },
+    ]
+  },
+  {
+    id: 7,
     name: 'Falling Cube',
     avatar: '/logos/projects/fallingcube.png',
     description: '<span class="red">Игра</span>, в которой Вы играете за куб. Ваша цель - упасть вниз, избегая препятствий.',
     isActive: false,
-    shadowColor: 'rgba(50, 100, 255, 0.7)',
+    shadowColor: 'rgba(50, 100, 255, 0.1)',
     buttons: [
       { label: 'GitHub', url: 'https://github.com/Payziii/falling-cube', icon: '/logos/github.png' },
       { label: 'Скачать', url: 'https://payziii.itch.io/falling-cube', icon: '/logos/download.png' }
     ]
   },
+  {
+    id: 8,
+    name: 'AI Pixel Battle',
+    avatar: '/logos/projects/pixel.png',
+    description: '<span class="red">Пиксель баттл</span>, написанный ИИ.',
+    isActive: false,
+    shadowColor: 'rgba(50, 100, 255, 0.1)',
+    buttons: [
+      { label: 'GitHub', url: 'https://github.com/Payziii/AIPixelBattle', icon: '/logos/github.png' }
+    ]
+  },
+  {
+    id: 9,
+    name: 'За стеной',
+    avatar: '/logos/projects/zastenoi.png',
+    description: '<span class="red">Игра-головоломка</span> про кота, ищущего выход ОТСЮДА...',
+    isActive: false,
+    shadowColor: 'rgba(50, 100, 255, 0.1)',
+    buttons: [
+      { label: 'Играть', url: 'https://vk.com/app51743844', icon: '/logos/site.png' }
+    ]
+  },
+  {
+    id: 10,
+    name: 'SoftShelf',
+    avatar: '/logos/projects/softshelf.png',
+    description: '<span class="red">Платформа</span> для покупки/продажи цифровых товаров внутри Telegram.',
+    isActive: false,
+    shadowColor: 'rgba(50, 100, 255, 0.1)',
+    buttons: [
+      { label: 'Telegram-канал', url: 'https://t.me/SoftShelf', icon: '/logos/tg.png' }
+    ]
+  },
+  {
+    id: 11,
+    name: 'FiftyAPI',
+    avatar: '/logos/projects/fiftyapi.png',
+    description: '<span class="red">API</span> с играми, квизами и прочими вещами.',
+    isActive: false,
+    shadowColor: 'rgba(50, 100, 255, 0.1)',
+    buttons: []
+  },
+  {
+    id: 12,
+    name: 'CalculatorGame',
+    avatar: '/logos/projects/calculator.png',
+    description: 'Самый обычный <span class="red">кликер</span>.',
+    isActive: false,
+    shadowColor: 'rgba(50, 100, 255, 0.1)',
+    buttons: [
+      { label: 'GitHub', url: 'https://github.com/Payziii/CalculatorGame', icon: '/logos/github.png' },
+      { label: 'Скачать', url: 'https://payziii.itch.io/calculatorgame', icon: '/logos/download.png' }
+    ]
+  },
+  {
+    id: 13,
+    name: 'FiftyCube',
+    avatar: '/logos/projects/fiftycube.png',
+    description: '<span class="red">Головоломка</span>, суть которой - вытолкнуть куб из игрового поля любой ценой.',
+    isActive: false,
+    shadowColor: 'rgba(50, 100, 255, 0.1)',
+    buttons: [
+      { label: 'Скачать', url: 'https://payziii.itch.io/fiftycube', icon: '/logos/download.png' }
+    ]
+  },
+  {
+    id: 14,
+    name: 'DuoBot',
+    avatar: '/logos/projects/noava.png',
+    description: '<span class="red">Telegram-бот</span>, отображающий информацию об аккаунте Duolingo.',
+    isActive: false,
+    shadowColor: 'rgba(50, 100, 255, 0.1)',
+    buttons: [
+      { label: 'GitHub', url: 'https://github.com/Payziii/DuoBot', icon: '/logos/github.png' }
+    ]
+  },
+  {
+    id: 15,
+    name: 'WeatherBot',
+    avatar: '/logos/projects/noava.png',
+    description: '<span class="red">Telegram-бот</span> с информацией о погоде в выбранном городе.',
+    isActive: false,
+    shadowColor: 'rgba(50, 100, 255, 0.1)',
+    buttons: [
+      { label: 'GitHub', url: 'https://github.com/Payziii/WeatherBot', icon: '/logos/github.png' }
+    ]
+  }
 ])
 </script>
 
